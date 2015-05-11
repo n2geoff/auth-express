6a3924b2c4983c31b14d982494ef5a16c417a112
Geoff Doty <n2geoff@gmail.com>
2015-05-11 19:55:13 -0400

    code now acts as middleware

--- index.js
+++ index.js
@@ -1,28 +1,59 @@
+'use strict';
 
-/**
- * Parse the Authorization header field of `req`.
- *
- * @param {Request} req
- * @return {Object} with .name and .pass
- * @api public
- */
+var _user = {};
+var _realm = '';
 
-module.exports = function(req){
-  req = req.req || req;
+function unauthorized(res) {
+    realm = _realm || 'My Realm';
 
-  var auth = req.headers.authorization;
-  if (!auth) return;
+    res.set('WWW-Authenticate', 'Basic realm="' + realm + '"');
+    res.status(401).end('Unauthorized');
+}
 
-  // malformed
-  var parts = auth.split(' ');
-  if ('basic' != parts[0].toLowerCase()) return;
-  if (!parts[1]) return;
-  auth = parts[1];
+exports.required = function(req, res, next) {
+    var input = null;
+    var stored = null;
 
-  // credentials
-  auth = new Buffer(auth, 'base64').toString();
-  auth = auth.match(/^([^:]*):(.*)$/);
-  if (!auth) return;
+    if (!_user.username || !_user.password) return unauthorized(res);
 
-  return { name: auth[1], pass: auth[2] };
+    if (!req.user.username || !req.user.password) return unauthorized(res);
+
+    input = new Buffer(_user.username + ':' + _user.password).toString('base64');
+    stored = new Buffer(req.user.username + ':' + req.user.password).toString('base64');
+
+    if (input !== stored) return unauthorized(res);
+
+    next();
+};
+
+exports.basic = function(username, password, realm) {
+    _user.username = username || null;
+    _user.password = password || null;
+    _realm         = realm    || null;
+
+    return function(req, res, next) {
+        req = req.req || req;
+        req.user = {};
+
+        var auth = req.headers.authorization;
+        if (!auth) return next();
+
+        // malformed
+        var parts = auth.split(' ');
+        if ('basic' != parts[0].toLowerCase()) return next();
+        if (!parts[1]) return next();
+        auth = parts[1];
+
+        // credentials
+        auth = new Buffer(auth, 'base64').toString();
+        auth = auth.match(/^([^:]*):(.*)$/);
+        if (!auth) return next();
+
+        req.user = {
+            username: auth[1],
+            password: auth[2]
+        };
+
+        return next();
+    };
 };
